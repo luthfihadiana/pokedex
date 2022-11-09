07c31b52d4e538f492e6da049fff6f49c0043302
Luthfi Hadiana <luthfihadiana@gmail.com>
2022-11-09 07:59:32 +0700

    add initial graphql, google font, and global style

---
+++ graphql/query/getPokemons.ts
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+export default gql`
+  query POKEMONS {
+    pokemons: pokemon_v2_pokemonspecies(limit: 10) {
+      name
+      id
+      details: pokemon_v2_pokemons {
+        types: pokemon_v2_pokemontypes {
+          type: pokemon_v2_type {
+            name
+          }
+        }
+      }
+    }
+    aggregate: pokemon_v2_pokemonspecies_aggregate {
+      aggregate {
+        count
+      }
+    }
+  }
+`;
